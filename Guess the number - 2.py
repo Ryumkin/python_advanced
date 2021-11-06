4bfd227d01b87e07cb34e75f7ce6c347ab2bb966
svyat <svyat94@mail.ru>
2021-11-06 17:56:03 +0300

    task 2

--- Guess the number - 2.py
+++ Guess the number - 2.py
@@ -4,13 +4,14 @@ answers = []
 while True:
     guess = input()
     if guess == 'HELP':
+        print(*answers, sep="\n", end="\n")
+        print(*sorted(possible_nums))
         break
     guess_set = {int(x) for x in guess.split()}
-    if len(guess_set) <= len(possible_nums) // 2:
+    guess_set &= possible_nums
+    if len(guess_set) <= len(possible_nums) / 2:
         possible_nums -= guess_set
         answers.append("NO")
     else:
         possible_nums &= guess_set
         answers.append("YES")
-print(*answers, sep="\n", end="\n")
-print(*sorted(possible_nums))
