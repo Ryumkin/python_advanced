4ce3cd5afe60863bdd855c17d2466c27d6536f87
svyat <svyat94@mail.ru>
2021-11-06 23:02:20 +0300

    task 3

---
+++ polyglots_2.py
@@ -0,0 +1,13 @@
+number_of_students = int(input())
+languages = set()
+
+for index_of_student in range(0, number_of_students):
+    number_of_languages = int(input())
+    local_languages = {input() for i in range(0, number_of_languages)}
+    if index_of_student == 0:
+        languages = local_languages
+    else:
+        languages |= local_languages
+
+print(len(languages))
+print(*sorted(languages), sep="\n", end="")
