979b7b786b8a36ae752d37313ee37e8268199c67
svyat <svyat94@mail.ru>
2021-11-26 10:51:30 +0300

    crawler.py

--- week3/crawler/crawler.py
+++ week3/crawler/crawler.py
@@ -18,3 +18,28 @@ with open(input()) as reader:
     urls = []
     get_url(json_items, "", urls)
     print(*sorted(urls), sep="\n", end="")
+
+"""
+import json
+
+
+in_filename = input()
+with open(in_filename, 'r') as f:
+    web_index = json.load(f)
+
+web_pages = []
+stack = [
+    (site_domain, section)
+    for site_domain, section in web_index.items()
+]
+
+while stack:
+    site_domain, section = stack.pop()
+    if section:
+        for a, b in section.items():
+            stack.append((f'{site_domain}/{a}', b))
+    else:
+        web_pages.append(site_domain)
+
+print(*sorted(web_pages), sep='\n')
+"""
