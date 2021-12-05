3bfe3896809d01ec6caef21c64e261c36c443312
svyat <svyat94@mail.ru>
2021-12-05 19:43:05 +0300

    part 2

--- course_project/inverted_index.py
+++ course_project/inverted_index.py
@@ -1,4 +1,4 @@
-import json
+import argparse
 import pickle
 
 
@@ -32,3 +32,40 @@ class InvertedIndex:
     def load(cls, filepath):
         obj = pickle.load(open(filepath, 'rb'))
         return obj
+
+
+def build(args):
+    articles = load_document(args.dataset)
+    inv_index = build_inverted_index(articles)
+    inv_index.dump(args.index)
+
+
+def query(args):
+    inv_index = InvertedIndex.load(args.index)
+    with open(args.query_file, 'r', encoding="utf-8") as f:
+        for line in f:
+            print(*sorted(inv_index.query(line.split())), sep=",", end="\n")
+
+
+def parse(command_line):
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='command')
+    build_parser = subparsers.add_parser('build')
+    build_parser.add_argument('--dataset', type=str)
+    build_parser.add_argument('--index', type=str)
+    build_parser.set_defaults(function=build)
+
+    query_parser = subparsers.add_parser('query')
+    query_parser.add_argument('--index', type=str)
+    query_parser.add_argument('--query_file', type=str)
+    query_parser.set_defaults(function=query)
+    return parser.parse_args(command_line)
+
+
+def main(command_line=None):
+    parsed_command = parse(command_line)
+    parsed_command.function(parsed_command)
+
+
+if __name__ == '__main__':
+    main()
